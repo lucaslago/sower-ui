a8f8cf0b0fad31b52667647a62bc5f2e34f1f246
Lucas Lago <lucasbenderlago@gmail.com>
2016-12-05 17:03:59 -0200

    Add material-ui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <AppBar title="Sower"
+                iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
+      </MuiThemeProvider>
     );
   }
 }
