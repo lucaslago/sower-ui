3a61c4c6e968d03700fde5da07fd3597e4086af7
Juraci Vieira <juraci.vieira@gmail.com>
2016-12-07 16:43:31 -0200

    Adding AsyncButton component

---
+++ src/components/AsyncButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import CircularProgress from 'material-ui/CircularProgress';
+
+const AsyncButton = props => {
+  if(props.makingRequest) {
+    return (<CircularProgress />);
+  }
+  return (
+    <RaisedButton label={ props.label } type={ props.type }/>
+  );
+};
+
+export default AsyncButton;
---
+++ src/components/AsyncButton.test.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import AsyncButton from './AsyncButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import CircularProgress from 'material-ui/CircularProgress';
+
+describe('<AsyncButton />', () => {
+
+  it('renders without crashing', () => {
+    const wrapper = shallow(
+        <AsyncButton label="login"  />
+    );
+    expect(wrapper.length).toEqual(1);
+  });
+
+  it('renders <RaisedButton /> when makingRequest is false', () => {
+    const wrapper = shallow(
+        <AsyncButton label="login" makingRequest={ false } />
+    );
+    expect(wrapper.find(RaisedButton).length).toEqual(1);
+    expect(wrapper.find(CircularProgress).length).toEqual(0);
+  });
+
+  it('renders <CircularProgress /> when makingRequest is true', () => {
+    const wrapper = shallow(
+        <AsyncButton label="login" makingRequest={ true } />
+    );
+    expect(wrapper.find(RaisedButton).length).toEqual(0);
+    expect(wrapper.find(CircularProgress).length).toEqual(1);
+  });
+
+});
