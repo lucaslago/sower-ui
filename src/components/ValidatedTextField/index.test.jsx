dee9f37bf2f5dadcfd1e7bd63fccfe2a28238a8a
Juraci Vieira <juraci.vieira@gmail.com>
2016-12-09 14:17:05 -0200

    testing error message

--- src/components/ValidatedTextField/index.test.jsx
+++ src/components/ValidatedTextField/index.test.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import ValidatedTextField from './index';
 import TextField from 'material-ui/TextField';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 describe('<ValidatedTextField />', () => {
 
@@ -12,16 +15,33 @@ describe('<ValidatedTextField />', () => {
     expect(wrapper.find('.validated-text-field').length).toEqual(1);
   });
 
-  it('should not display error when focused', () => {
-    const setValueFake = () => {};
+  it('should not display error when showValidationError is false', () => {
+    const handleChangeFake = () => {};
     const wrapper = shallow(
         <ValidatedTextField
           label="login"
+          type="text"
           errorText="Field required"
-          setValue={ setValueFake }
+          showValidationError={ false }
+          handleChange = { handleChangeFake }
         />
         );
-    wrapper.find('.validated-text-field').simulate('click');
     expect(wrapper.find('.validated-text-field').text()).not.toContain('Field required');
+  });
+
+  it('should display error when showValidationError is true', () => {
+    const handleChangeFake = () => {};
+    const wrapper = render(
+      <MuiThemeProvider>
+        <ValidatedTextField
+          label="login"
+          type="text"
+          errorText="Field required"
+          showValidationError={ true }
+          handleChange = { handleChangeFake }
+        />
+      </MuiThemeProvider>
+        );
+    expect(wrapper.find('.validated-text-field').text()).toContain('Field required');
   });
 });
