f39c82701f9ec741707ce2c0a5af4235affe28ce
Lucas Lago <lucasbenderlago@gmail.com>
2016-12-13 15:01:24 -0200

    Fix required propType warning

--- src/App/index.test.jsx
+++ src/App/index.test.jsx
@@ -2,7 +2,14 @@ import App from './index';
 
 describe('<App/>', () => {
   it('renders without crashing', () => {
-    const wrapper = mount(<App />);
+    const wrapper = mount(
+      <App> 
+        <div>
+          <span> mock </span>
+          <span> component </span>
+        </div>
+      </App>
+    );
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
