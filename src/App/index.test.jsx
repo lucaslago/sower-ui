9f77b033ec772149240039c7b191007a6531301a
Lucas Lago <lucasbenderlago@gmail.com>
2016-12-13 15:04:59 -0200

    Fix lint warning

--- src/App/index.test.jsx
+++ src/App/index.test.jsx
@@ -3,12 +3,12 @@ import App from './index';
 describe('<App/>', () => {
   it('renders without crashing', () => {
     const wrapper = mount(
-      <App> 
+      <App>
         <div>
           <span> mock </span>
           <span> component </span>
         </div>
-      </App>
+      </App>,
     );
     expect(toJson(wrapper)).toMatchSnapshot();
   });
