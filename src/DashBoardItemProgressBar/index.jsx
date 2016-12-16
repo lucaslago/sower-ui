06a5d974f1f89bc24b6e29165eea4d2a802309cf
Lucas Lago <lucasbenderlago@gmail.com>
2016-12-16 17:48:31 -0200

    Fix lint warnings

--- src/DashBoardItemProgressBar/index.jsx
+++ src/DashBoardItemProgressBar/index.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import Subheader from 'material-ui/Subheader';
 import LinearProgress from 'material-ui/LinearProgress';
 
-const DashboardItemProgressBar = () => {
-  return (
-    <div className="dashboard-item-progress-bar">
-      <Subheader>7050 / 8552</Subheader>
-      <LinearProgress mode="determinate" value={70} />
-    </div>
-  )
-};
+const DashboardItemProgressBar = () => (
+  <div className="dashboard-item-progress-bar">
+    <Subheader>7050 / 8552</Subheader>
+    <LinearProgress mode="determinate" value={70} />
+  </div>
+  );
 
 export default DashboardItemProgressBar;
