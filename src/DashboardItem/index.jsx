f1121e702a0ad76a530fc2ad3e3e8e8e50e051bd
Lucas Lago <lucasbenderlago@gmail.com>
2016-12-19 19:12:20 -0200

    Create a Container Component

--- src/DashboardItem/index.jsx
+++ src/DashboardItem/index.jsx
@@ -24,6 +24,7 @@ export default class DashboardItem extends Component {
       expanded: this.props.expanded,
       startDisabled: this.props.startDisabled,
       stopDisabled: this.props.stopDisabled,
+      simulationStatus: {},
       notification: false,
       notificationMessage: '',
       dialogOpen: false,
---
+++ src/DashboardItemContainer/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Row } from 'react-bootstrap';
+import DashboardItem from '../DashboardItem';
+
+const DashboardItemContainer = ({ device, authService, simulationService }) => {
+  return (
+    <Row>
+      <DashboardItem
+        title={device.relationships.equipment.data.description}
+        trackerId={device.id}
+        authService={authService}
+        simulationService={simulationService}
+        expanded={false}
+        startDisabled={!device.relationships.equipment.data.default_simulation}
+        stopDisabled
+      />
+    </Row>
+  );
+};
+DashboardItemContainer.propTypes = {
+  device: React.PropTypes.shape({
+    relationships: React.PropTypes.shape({
+      equipment: React.PropTypes.shape({
+        data: React.PropTypes.shape({
+          description: React.PropTypes.string.isRequired
+        })
+      })
+    })
+  }),
+  authService: React.PropTypes.shape({
+    login: React.PropTypes.func.isRequired,
+    loggedIn: React.PropTypes.func.isRequired,
+    getToken: React.PropTypes.func.isRequired,
+    logout: React.PropTypes.func.isRequired
+  }),
+  simulationService: React.PropTypes.shape({
+    start: React.PropTypes.func.isRequired,
+    stop: React.PropTypes.func.isRequired,
+    status: React.PropTypes.func.isRequired
+  })
+}
+export default DashboardItemContainer;
