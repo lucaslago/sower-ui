f25bbadb538a0f6aba23af2d5cc7edd8f90fae79
Juraci Vieira <juraci.vieira@gmail.com>
2016-12-21 16:42:22 -0200

    validating the fields

--- src/CustomSimulationDialog/index.jsx
+++ src/CustomSimulationDialog/index.jsx
@@ -15,23 +15,51 @@ export default class CustomSimulationDialog extends Component {
       descriptionValue: '',
       descriptionDisabled: false,
       descriptionError: false,
+      positionsValue: '',
+      positionsDisabled: false,
+      positionsErrorText: '',
+      submitDisabled: true,
     };
 
     this.setInterval = this.setInterval.bind(this);
     this.setDescription = this.setDescription.bind(this);
+    this.setPositions = this.setPositions.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   setInterval(event) {
-    const error = event.target.value ? false : true;
-
+    const value = event.target.value;
     this.setState({
-      intervalValue: event.target.value,
-      intervalError: error,
+      intervalValue: value,
+      intervalError: value ? false : true,
+      submitDisabled: !(value && this.state.positionsValue),
     });
   }
 
   setDescription(event) {
     this.setState({ descriptionValue: event.target.value });
+  }
+
+  setPositions(event) {
+    const value = event.target.value;
+    this.setState({
+      positionsValue: value,
+      positionsErrorText: value ? '' : 'Positions is required',
+      submitDisabled: !(value  && this.state.intervalValue),
+    });
+  }
+
+  isValidForm() {
+    return !this.state.positionsValue || !this.state.intervalValue;
+  }
+
+  handleSubmit() {
+    if (this.isValidForm) {
+      return this.setState({
+        intervalError: true,
+        positionsErrorText: 'Positions is required',
+      });
+    }
   }
 
   render() {
@@ -44,8 +72,8 @@ export default class CustomSimulationDialog extends Component {
       <FlatButton
         label="Submit"
         primary={true}
-        disabled={true}
-        onTouchTap={this.props.handleClose}
+        disabled={this.state.submitDisabled}
+        onTouchTap={this.handleSubmit}
       />,
     ];
 
@@ -57,34 +85,37 @@ export default class CustomSimulationDialog extends Component {
           modal={true}
           open={this.props.open}
         >
-          <form>
-            <ValidatedTextField
-              label="Interval (milliseconds)"
-              type="number"
-              fullWidth={false}
-              fieldValue={this.state.intervalValue}
-              disabled={this.state.intervalDisabled}
-              errorText="Interval is required"
-              showValidationError={this.state.intervalError}
-              handleChange={this.setInterval}
-            /><br/>
-            <ValidatedTextField
-              label="Description"
-              type="text"
-              fullWidth={false}
-              fieldValue={this.state.descriptionValue}
-              disabled={this.state.descriptionDisabled}
-              showValidationError={this.state.descriptionError}
-              handleChange={this.setDescription}
-            /><br/>
-            <TextField
-              hintText="Array of positions [{...}, {...}, ...]"
-              multiLine={true}
-              rows={2}
-              rowsMax={5}
-              fullWidth={true}
-            />
-          </form>
+          <ValidatedTextField
+            label="Interval (milliseconds)"
+            type="number"
+            fullWidth={false}
+            fieldValue={this.state.intervalValue}
+            disabled={this.state.intervalDisabled}
+            errorText="Interval is required"
+            showValidationError={this.state.intervalError}
+            handleChange={this.setInterval}
+          /><br/>
+          <ValidatedTextField
+            label="Description"
+            type="text"
+            fullWidth={false}
+            fieldValue={this.state.descriptionValue}
+            disabled={this.state.descriptionDisabled}
+            showValidationError={this.state.descriptionError}
+            handleChange={this.setDescription}
+          /><br/>
+          <TextField
+            hintText="Array of positions [{...}, {...}, ...]"
+            multiLine={true}
+            rows={2}
+            rowsMax={5}
+            fullWidth={true}
+            disabled={this.state.positionsDisabled}
+            value={this.state.positionsValue}
+            errorText={this.state.positionsErrorText}
+            type="hidden"
+            onChange={this.setPositions}
+          />
         </Dialog>
       </div>
     );
