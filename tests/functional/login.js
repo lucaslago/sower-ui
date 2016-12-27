5131b8f2922ed1d41c4d709e7b311f52c830972e
Waldemar Neto <waldemarnt@gmail.com>
2016-12-27 18:06:45 -0200

    Add test case when login works as expected

--- tests/functional/login.js
+++ tests/functional/login.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'Login test' : browser => {
+  'Login should fail when credentials are invalid': browser => {
     browser
       .url(`${browser.launchUrl}/#/login`)
       .waitForElementVisible('body', 2000)
@@ -9,5 +9,16 @@ module.exports = {
       .waitForElementVisible('#root > div > div.container > div > div:nth-child(2) > div > div > span', 5000)
       .assert.containsText('#root > div > div.container > div > div:nth-child(2) > div > div > span', 'Incorrect email and/or password')
       .end();
+  },
+  'Login should work when credentials are valid': browser => {
+    browser
+    .url(`${browser.launchUrl}/#/login`)
+    .waitForElementVisible('body', 2000)
+    .setValue('input[type=email]', process.env.SOWER_USER_LOGIN)
+    .setValue('input[type=password]', process.env.SOWER_USER_PWD)
+    .click('button[type=submit]')
+    .waitForElementVisible('.loading', 3000)
+    .waitForElementVisible('.Dashboard', 10000)
+    .end();
   }
 };
