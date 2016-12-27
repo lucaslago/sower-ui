d6bd95d1645e0d1c7622f7d809125ecb4fb76ac4
Travis CI <wneto@thoughtworks.com>
2016-12-27 14:57:03 -0200

    Configures nightwatch to support multiple environments

--- tests/functional/login.js
+++ tests/functional/login.js
@@ -1,12 +1,12 @@
 module.exports = {
-  'Login test' : function (browser) {
+  'Login test' : browser => {
     browser
-      .url(`http://localhost:3000/#/login`)
+      .url(`${browser.launchUrl}:3000/#/login`)
       .waitForElementVisible('body', 2000)
       .setValue('input[type=email]', 'test-email@gmail.com')
       .setValue('input[type=password]', 'fakepassword')
       .click('button[type=submit]')
-      .pause(2000)
+      .waitForElementVisible('#root > div > div.container > div > div:nth-child(2) > div > div > span', 5000)
       .assert.containsText('#root > div > div.container > div > div:nth-child(2) > div > div > span', 'Incorrect email and/or password')
       .end();
   }
