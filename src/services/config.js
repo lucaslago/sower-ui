8beff521764b27ff81db8e692b56d47826208856
Juraci Vieira <juraci.vieira@gmail.com>
2016-12-28 15:34:35 -0200

    wrong url

--- src/services/config.js
+++ src/services/config.js
@@ -1,3 +1,3 @@
 export const IAM_URL = process.env.REACT_APP_IAM_URL || 'https://agco-iam-test.herokuapp.com';
 export const WHO_AM_I_URL = `${IAM_URL}/whoAmI`;
-export const SOWER_URL = process.env.REACT_APP_SOWER_URL || 'http://localhost:8080';
+export const SOWER_URL = process.env.REACT_APP_SOWER_URL || 'https://telemetry-sower.herokuapp.com';
