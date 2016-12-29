b6ad0db85837bf1442cd02ce195c2a3f063bcc72
Juraci Vieira <juraci.vieira@gmail.com>
2016-12-29 11:21:41 -0200

    adding tests for the utils

--- src/DashboardItem/utils/index.test.js
+++ src/DashboardItem/utils/index.test.js
@@ -1,26 +1,32 @@
-import { shouldDisableStartBtn, shouldDisableStopBtn, shouldExpandCard } from './index';
+import {
+  shouldDisableStartBtn,
+  shouldDisableStopBtn,
+  shouldExpandCard,
+  hasDefaultSimulationSet,
+  hasCustomSimulation,
+} from './index';
 
 describe('DashboardItem utils', () => {
-  context('shouldDisableStartBtn', () => {
-    let device;
+  let device;
 
-    beforeEach(() => {
-      device = {
-        custom_simulation: false,
-        id: '123',
-        relationships: {
-          equipment: {
-            data: {
-              default_simulation: false,
-            },
+  beforeEach(() => {
+    device = {
+      custom_simulation: false,
+      id: '123',
+      relationships: {
+        equipment: {
+          data: {
+            default_simulation: false,
           },
         },
-        simulationStatus: {
-          status: undefined,
-        },
-      };
-    });
+      },
+      simulationStatus: {
+        status: undefined,
+      },
+    };
+  });
 
+  context('shouldDisableStartBtn', () => {
     context('simulationStatus is active', () => {
       it('should always return true', () => {
         device.simulationStatus.status = 'active';
@@ -87,6 +93,30 @@ describe('DashboardItem utils', () => {
     it('should return false when simulation_status is inactive', () => {
       const simulationStatus = 'inactive';
       expect(shouldExpandCard(simulationStatus)).toEqual(false);
+    });
+  });
+
+  context('hasDefaultSimulationSet', () => {
+    it('returns true when the device has default simulation', () => {
+      device.relationships.equipment.data.default_simulation = true;
+      expect(hasDefaultSimulationSet(device)).toBe(true);
+    });
+
+    it('returns false when the device has no default simulation', () => {
+      device.relationships.equipment.data.default_simulation = false;
+      expect(hasDefaultSimulationSet(device)).toBe(false);
+    });
+  });
+
+  context('hasCustomSimulation', () => {
+    it('returns true when the device has custom simulation', () => {
+      device.custom_simulation = true;
+      expect(hasCustomSimulation(device)).toBe(true);
+    });
+
+    it('returns false when the device has no custom simulation', () => {
+      device.custom_simulation = false;
+      expect(hasCustomSimulation(device)).toBe(false);
     });
   });
 });
